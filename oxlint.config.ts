1a02f1a176ce405f05e3c8ce4e373a392767bf4f
Nikhil S <nikhilsnayak3473@gmail.com>
2026-04-13 21:33:09 +0530

    init

---
+++ oxlint.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'oxlint';
+
+export default defineConfig({
+  ignorePatterns: ['**/routeTree.gen.ts'],
+  plugins: [
+    'eslint',
+    'typescript',
+    'unicorn',
+    'react',
+    'react-perf',
+    'oxc',
+    'import',
+    'jsx-a11y',
+    'promise',
+    'node',
+  ],
+  options: {
+    typeAware: true,
+    typeCheck: true,
+  },
+});
